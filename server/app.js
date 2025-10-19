326b4693198e6136f164b4646949068ca97abc3d
Manish <manish2003sharma@gmail.com>
2025-10-19 16:51:28 +0530

    server

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const cors = require('cors'); 
+const authRoutes = require('./routes/authRoutes');
+const cors = require('cors');
 
 dotenv.config();
 require('./db/connect');
@@ -10,6 +11,7 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
+app.use('/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
